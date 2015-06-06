7cad0f774be77642c6a3df9f5f9ebbfa2ff5cc35
Claudia Bedoya <claudiabm777@gmail.com>
2015-06-05 22:00:33 -0500

    Just the getSemester method

--- app/controllers/SemesterController.java
+++ app/controllers/SemesterController.java
@@ -1,10 +1,28 @@
 package controllers;
 
+import models.Semester;
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
 import views.html.*;
+
+import java.util.List;
+
 /**
  * Created by Asus on 04/06/2015.
  */
 public class SemesterController extends Controller {
+
+    /**
+     * This method returns all semesters created in the system.
+     * @return
+     */
+    public Result getSemesters(){
+        try{
+            List<Semester> semesters=Semester.find.all();
+            return ok(Json.toJson(semesters));
+        }catch (Throwable e){
+            return badRequest(e.getMessage());
+        }
+    }
 }
