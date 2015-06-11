89e80087a2edb087434435df3e5844771503d275
Claudia Bedoya <claudiabm777@gmail.com>
2015-06-11 12:09:21 -0500

    New methods edit delet Student

--- app/controllers/StudentController.java
+++ app/controllers/StudentController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import Exceptions.ErrorMessage;
 import Exceptions.StudentException;
+import com.avaje.ebean.Ebean;
+import com.fasterxml.jackson.databind.JsonNode;
 import models.Student;
 import play.*;
 import play.libs.Json;
@@ -45,5 +47,51 @@ public class StudentController extends Controller {
         }
     }
 
+    /**
+     *
+     * @return
+     */
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result editStudentId(){
+        try{
+            JsonNode newStudent = Controller.request().body().asJson();
+            Long oldId=newStudent.findPath("oldId").asLong();
+            Student student = Student.transformJson(newStudent);
+            Student student1 = Student.find.byId(oldId);
+            if (student1 == null) {
+                throw new StudentException(ErrorMessage.NOT_CREATED);
+            }
+            student1.setCareer(student.getCareer());
+            student1.setCode(student.getCode());
+            student1.setComplSection(student.getComplSection());
+            student1.setEmail(student.getEmail());
+            student1.setLastNames(student.getLastNames());
+            student1.setMagisSection(student.getMagisSection());
+            student1.setNames(student.getNames());
+            student1.update();
+            return ok();
+        }catch (Throwable e){
+            return badRequest(e.getMessage());
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    @BodyParser.Of(BodyParser.Json.class)
+    public Result deleteStudent(){
+        try{
+            Long idStudent = Controller.request().body().asJson().findPath("idStudent").asLong();
+            Student student=Student.find.byId(idStudent);
+            if(student==null){
+                throw new StudentException( ErrorMessage.NOT_CREATED);
+            }
+            student.delete();
+            return ok();
+        }catch (Throwable e){
+            return badRequest(e.getMessage());
+        }
+    }
 
 }
