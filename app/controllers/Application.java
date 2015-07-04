d5685151f92f23704900b35a0175b39feedf5409
Claudia Bedoya <claudiabm777@gmail.com>
2015-07-03 20:00:44 -0500

    final home interface

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,21 @@
 package controllers;
 
 import play.*;
+import play.data.DynamicForm;
 import play.mvc.*;
 
 import views.html.*;
 
+import static play.data.Form.form;
+
 public class Application extends Controller {
 
     public Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+    public Result index2() {
+        DynamicForm bindedForm = form().bindFromRequest();
+        System.out.println(bindedForm.get("email")+"  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return ok(index.render("Your new application is ready."));
     }
 
